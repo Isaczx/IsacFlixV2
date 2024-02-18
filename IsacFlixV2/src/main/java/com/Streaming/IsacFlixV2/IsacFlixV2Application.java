da82368698d56e265ccbd0803e46a92b7b9514a0
Isac Santos <118670709+Isaczx@users.noreply.github.com>
2024-02-18 11:37:06 -0300

    primeiro commit

---
+++ IsacFlixV2/src/main/java/com/Streaming/IsacFlixV2/IsacFlixV2Application.java
@@ -0,0 +1,13 @@
+package com.Streaming.IsacFlixV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsacFlixV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsacFlixV2Application.class, args);
+	}
+
+}
