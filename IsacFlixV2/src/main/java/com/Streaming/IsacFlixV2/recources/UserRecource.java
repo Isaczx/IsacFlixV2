071f09c3745d56fe07ce19ee9be113ffd2678442
Isac Santos <isacjobz@gmail.com>
2024-02-18 17:09:32 -0300

    UserService
    Foi adicionado o service do usar e colocado endpoints para buscar todos os usuarios e buscar usuarios por id

--- IsacFlixV2/src/main/java/com/Streaming/IsacFlixV2/recources/UserRecource.java
+++ IsacFlixV2/src/main/java/com/Streaming/IsacFlixV2/recources/UserRecource.java
@@ -1,20 +1,50 @@
 package com.Streaming.IsacFlixV2.recources;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Streaming.IsacFlixV2.entities.User;
+import com.Streaming.IsacFlixV2.services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserRecource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User isac = new User("isac" , "isacjobz@gmail.com", "174.137.857-57", "IsacSantos");
-		return ResponseEntity.ok().body(isac);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> finfById(@PathVariable Integer id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
